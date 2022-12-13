87e623e3ef3bba31e83d0c00b94858741e6d9553
TaylanPostalci <tay.postal@gmail.com>
2022-12-12 20:43:54 -0500

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,10 @@
 class Student < ApplicationRecord
 
+  # def index
+  #   students = Student.all
+  #   render json: students , status: :ok
+  # end
+
   def to_s
     "#{first_name} #{last_name}"
   end
